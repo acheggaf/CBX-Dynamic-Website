6371c092db92a5b4d29f25ec225e49b7e734036e
acheggaf <99760540+acheggaf@users.noreply.github.com>
2022-10-27 22:05:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,24 @@
 /* eslint-env node, es6 */
 
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
+const http = require('http');
+const https = require('https');
 
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/cbxsound.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/cbxsound.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/cbxsound.com/chain.pem', 'utf8');
 
 const PORT = 443
 
 const app = express()
 
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
 
 app.use(express.static(__dirname));
 
@@ -47,10 +58,13 @@ app.get('/About', (req, res) => {
 });
 
 
-app.listen(PORT, () => {
-    console.log("Serveur démaré")
-})
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
 
-app.listen(80, () => {
-    console.log("Serveur démaré")
-})
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
